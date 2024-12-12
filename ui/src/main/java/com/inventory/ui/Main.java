b1a18ad01cb0e555efcf219a09b0fcef67d835e0
YassirJr <yassir.junior.03@gmail.com>
2024-12-12 17:18:43 +0100

    create login page UI

--- ui/src/main/java/com/inventory/ui/Main.java
+++ ui/src/main/java/com/inventory/ui/Main.java
@@ -1,21 +1,32 @@
 package com.inventory.ui;
 
+import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Inventory management!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage stage) {
+        try {
+            Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
 
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles/auth.css")).toExternalForm());
+
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
