96b7aa5b5c54d76b3e7fc41d80316d2a527579ea
HakimMohamed <mohamed.hakim.dev@gmail.com>
2024-12-24 20:10:09 +0100

    gateway java

--- backend/src/main/java/com/inventory/backend/BackendApplication.java
+++ backend/src/main/java/com/inventory/backend/BackendApplication.java
@@ -1,11 +1,13 @@
 package com.inventory.backend;
 
+import com.inventory.backend.controllers.BlockchainController;
 import com.inventory.backend.entities.Category;
 import com.inventory.backend.entities.Organization;
 import com.inventory.backend.entities.Product;
 import com.inventory.backend.entities.User;
 import com.inventory.backend.enums.OrganizationType;
 import com.inventory.backend.enums.UserRole;
+import com.inventory.backend.metier.BlockchainMetierImpl;
 import com.inventory.backend.services.CategoryService;
 import com.inventory.backend.services.OrganizationService;
 import com.inventory.backend.services.ProductService;
@@ -22,5 +24,17 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner testBlockchain() {
+        return args -> {
+            System.out.println("Test blockchain");
+            BlockchainMetierImpl metier = new BlockchainMetierImpl();
+            BlockchainController controller = new BlockchainController(metier);
+            System.out.println("Init ledger");
+            controller.initLedger();
+            System.out.println("Get exchanges by organization");
+            System.out.println(controller.getExchangesByOrganization("Company"));
+        };
+    }
 
 }
